3e8b4fda6402093d3cfc14434182f2e3b8af63cc
bbusljeta <bush@DESKTOP-JG822FC.localdomain>
2021-01-16 13:32:58 +0100

    add postcss-preset-env

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@
 module.exports = function override(config, env) {
     require('react-app-rewire-postcss')(config, {
         plugins: loader => [
+          require('postcss-preset-env')(),
           require('postcss-import')(),
           require('postcss-nested')(),
         ]
